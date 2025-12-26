b6e0ed7f80a500c8984ed906382cc9ccf1f72edc
hajdan42 <hajnaldani1@gmail.com>
2025-12-26 23:15:19 +0000

    got stuck figuring out range

---
+++ for.go
@@ -0,0 +1,24 @@
+package main
+
+import ("fmt"
+		"math/rand/v2")
+
+func main() {
+	var myval int
+	for{
+		myval=rand.IntN(10)
+		fmt.Println("HI", myval)
+		if(myval==0) {break}
+		}
+		
+	var i int
+	for(i<3){
+		fmt.Println(i)
+		i+=1
+		}
+	fmt.Printf("%T\n",(range 2))
+	
+	for j{
+		fmt.Println("a")
+		}
+}
